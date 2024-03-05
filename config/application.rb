2a61a4fe39f08a7de349f6b7c86a2b1cb450c733
hiro1129-2mc <cocom329@gmail.com>
2024-03-05 21:23:56 +0900

    ‘railsgenerate時に、helper/testファイル、ルーティングの記述が生成されないよう設定’

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,10 @@ module Findvinyl
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.generators do |g|
+      g.helper false
+      g.test_framework nil
+      g.routing_specs false
+    end
   end
 end
