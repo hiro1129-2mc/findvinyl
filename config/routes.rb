860257141e923653b15d6da0a8e27f5b09205ecd
hiro1129-2mc <cocom329@gmail.com>
2024-03-06 16:04:24 +0900

    ‘bundleexecrubocop-aを実施’

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
+  get 'up' => 'rails/health#show', as: :rails_health_check
 
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  root "static_pages#top"
+  root 'static_pages#top'
 end
