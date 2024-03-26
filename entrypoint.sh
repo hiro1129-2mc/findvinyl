8164d42967ba86688f5460e5dcb3cb35e221187e
hiro1129-2mc <cocom329@gmail.com>
2024-03-26 18:23:46 +0900

    ‘デプロイ先をherokuに変更’

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-set -e
 
-# Remove a potentially pre-existing server.pid for Rails.
-rm -f /findvinyl/tmp/pids/server.pid
+if [ "${RAILS_ENV}" = "production" ]; then
+    bundle exec rails assets:precompile
+fi
 
-# Then exec the container's main process (what's set as CMD in the Dockerfile).
-exec "$@"
+bundle exec rails s -p ${PORT:-3000} -b "0.0.0.0"
