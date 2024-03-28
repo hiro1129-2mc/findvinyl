dffc970ecad5762f2b887890f057bf7b20e17035
hiro1129-2mc <cocom329@gmail.com>
2024-03-29 00:27:14 +0900

    ‘モデルにアソシエーション追加/エラー修正’

--- app/models/record.rb
+++ app/models/record.rb
@@ -1,4 +1,6 @@
 class Record < ApplicationRecord
+  belongs_to :user
+
   has_many :record_items, dependent: :destroy
   has_many :items, through: :record_items
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,5 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true
 
   has_many :items, dependent: :destroy
+  has_many :records, dependent: :destroy
 end
