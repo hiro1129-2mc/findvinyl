304ed8ab20fe9391ba0da481add359ab3293622e
hiro1129-2mc <cocom329@gmail.com>
2024-04-19 21:52:02 +0900

    ‘artistモデル・releaseモデルの使用していないスコープを削除’

--- app/controllers/my_pages_controller.rb
+++ app/controllers/my_pages_controller.rb
@@ -3,7 +3,7 @@ class MyPagesController < ApplicationController
   before_action :set_user, only: %i[show]
 
   def show
-    @random = Item.where(user_id: @user.id, role: 0).order("RANDOM()").limit(5)
+    @random = Item.where(user_id: @user.id, role: 0).order('RANDOM()').limit(5)
   end
 
   private
