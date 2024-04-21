8fca22cff91f9d5283b3b3257c984ab49a2f992f
hiro1129-2mc <cocom329@gmail.com>
2024-04-21 22:52:21 +0900

    ‘fix:マイページのswiperが動かないバグ修正’

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,8 +2,10 @@ import { Application } from "@hotwired/stimulus"
 import MenuController from "./menu_controller"
 import ItemNewController from "./item_new_controller"
 import ItemEditController from "./item_edit_controller"
+import SwiperController from "./swiper_controller"
 
 const application = Application.start()
 application.register("menu", MenuController)
 application.register("item_new", ItemNewController)
 application.register("item_edit", ItemEditController)
+application.register("swiper", SwiperController)
