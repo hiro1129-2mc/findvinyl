632222e4164784f60c3aa445a44da6c41593725c
hiro1129-2mc <cocom329@gmail.com>
2024-08-31 05:17:59 +0900

    未ログイン時のエラー修正

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -5,11 +5,15 @@ class UserSessionsController < ApplicationController
     @user = login(params[:email], params[:password])
 
     if @user
-      redirect_to root_path, notice: t('user_sessions.create.success')
+      redirect_to after_login_path_for(@user), notice: t('user_sessions.create.success')
     else
       flash.now[:danger] = t('user_sessions.create.failure')
       render :new, status: :unprocessable_entity
     end
+  end
+
+  def after_login_path_for(_resource)
+    session[:previous_url] || root_path
   end
 
   def destroy
