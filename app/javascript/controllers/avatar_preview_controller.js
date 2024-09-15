cd35ef5abf621d852427681167c3962ee79daa1c
hiro1129-2mc <cocom329@gmail.com>
2024-09-15 17:46:27 +0900

    アイコン画像が正方形で保存されるよう変更

--- app/javascript/controllers/avatar_preview_controller.js
+++ app/javascript/controllers/avatar_preview_controller.js
@@ -3,6 +3,13 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["preview", "input"]
 
+  connect() {
+    this.previewTarget.style.width = "120px"
+    this.previewTarget.style.height = "120px"
+    this.previewTarget.style.objectFit = "cover"
+    this.previewTarget.style.objectPosition = "center"
+  }
+
   preview(event) {
     const file = this.inputTarget.files[0]
     const reader = new FileReader()
