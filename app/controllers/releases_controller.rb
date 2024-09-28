c7f429eb18550f00173bcb1254210c940c7f84f3
hiro1129-2mc <cocom329@gmail.com>
2024-09-28 13:08:13 +0900

    Fix:mediumがない場合のバグ修正

--- app/controllers/releases_controller.rb
+++ app/controllers/releases_controller.rb
@@ -2,7 +2,7 @@ class ReleasesController < ApplicationController
   skip_before_action :require_login, only: [:show]
 
   def show
-    @release = Release.find(params[:id])
+    @release = Release.includes(mediums: :medium_format).find(params[:id])
     @q = Release.ransack(params[:q])
 
     music_brainz_service = MusicBrainzService.new
