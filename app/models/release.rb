af461f672615443addde1eef51e5d9599b611410
hiro1129-2mc <cocom329@gmail.com>
2024-09-30 14:54:28 +0900

    コードの重複を解消

--- app/models/release.rb
+++ app/models/release.rb
@@ -7,12 +7,6 @@ class Release < ApplicationRecord
   validates :gid, presence: true, uniqueness: true
   validates :artist_credit_id, presence: true
 
-  def self.search_with_associations(name)
-    search_by_name(name)
-      .includes(artist_credit: :artist, mediums: :medium_format)
-      .references(:artist_credits, :medium_formats)
-  end
-
   def self.ransackable_attributes(_auth_object = nil)
     %w[name]
   end
