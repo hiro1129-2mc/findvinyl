11034245c20ef90a321df70a234c6591c1ba8508
hiro1129-2mc <cocom329@gmail.com>
2024-10-05 19:00:56 +0900

    menu_controller.jsにコメントを追加

--- app/javascript/controllers/menu_controller.js
+++ app/javascript/controllers/menu_controller.js
@@ -3,16 +3,19 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["sidebar"]
 
+  // メニューの表示/非表示を切り替える
   toggleMenu(event) {
-    event.stopPropagation(); // このイベントが親要素に伝播するのを防ぐ
+    // このイベントが親要素に伝播するのを防ぐ
+    event.stopPropagation();
     this.sidebarTarget.classList.toggle('translate-x-full');
   }
 
+  // サイドバーの×を押したらメニューを閉じる
   closeMenu() {
     this.sidebarTarget.classList.add('translate-x-full');
   }
 
-  // メニュー以外の場所をクリックしたら閉じる
+  // メニュー以外の場所を押したら閉じる
   closeMenuOutside(event) {
     if (!this.element.contains(event.target)) {
       this.sidebarTarget.classList.add('translate-x-full');
