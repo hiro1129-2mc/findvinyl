07fa94ba72b535077b5a2267772a0f72f141e034
hiro1129-2mc <cocom329@gmail.com>
2024-10-05 17:28:03 +0900

    MusicBrainzServiceのメールアドレスを環境変数化

--- app/services/music_brainz_service.rb
+++ app/services/music_brainz_service.rb
@@ -2,13 +2,13 @@ class MusicBrainzService
   require 'net/http'
   require 'uri'
 
-  # MusicBrainz APIのベースURL
   BASE_URL = 'https://musicbrainz.org/ws/2'.freeze
 
   def initialize
     @last_request_time = nil
   end
 
+  # artistsテーブルのgidを用いてアーティスト詳細情報を取得する
   def fetch_artist_details(gid)
     sleep_until_allowed
     url = "#{BASE_URL}/artist/#{gid}?fmt=json"
@@ -17,6 +17,7 @@ class MusicBrainzService
     JSON.parse(response.body) if response.is_a?(Net::HTTPSuccess)
   end
 
+  # releasesテーブルのgidを用いてリリース詳細情報を取得する
   def fetch_release_details(gid)
     sleep_until_allowed
     url = "#{BASE_URL}/release/#{gid}?inc=recordings&fmt=json"
@@ -27,15 +28,17 @@ class MusicBrainzService
 
   private
 
+  # APIリクエストを送信
   def request_to_musicbrainz_api(endpoint)
     uri = URI(endpoint)
     request = Net::HTTP::Get.new(uri)
-    request['User-Agent'] = 'VinylLog/1.0 ( cocom329@gmail.com )'
+    request['User-Agent'] = "VinylLog/1.0 ( #{ENV['MUSIC_BRAINZ_MAIL']} )"
     Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == 'https') do |http|
       http.request(request)
     end
   end
 
+  # レート制限対策(1秒に1リクエストまで)
   def sleep_until_allowed
     return unless @last_request_time
 
